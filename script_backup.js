cf79f4f40c2c38b9455d32b7628a0e47356075f3
David Harris <jdh.programmer@gmail.com>
2020-11-05 01:36:44 -0600

    changed backup.js... it's really just a file i copy and pasted pieces of code i didn't want to use or lose.

--- script_backup.js
+++ script_backup.js
@@ -73,3 +73,48 @@ function codingQuiz() {
 
     };
 };
+
+
+function waitListener(Element, ListenerName) {
+    return new Promise(function(resolve, reject) {
+        var listener = event => {
+            resolve(event);
+        };
+    });
+}
+
+async function codingQuiz() {
+
+    for (let i = 0; i < questionArray.length; i++) {
+        const currentQuestion = questionArray[i];
+
+        question = currentQuestion.question;
+        answer1 = currentQuestion.answers[0];
+        answer2 = currentQuestion.answers[1];
+        answer3 = currentQuestion.answers[2];
+        answer4 = currentQuestion.answers[3];
+        correctAnswer = currentQuestion.correctAnswerIndex;
+
+        let element = $(".answer");
+
+        await waitListener(element, "click")
+            .then(function answerResults() {
+                answerChoice = this.value;
+
+                console.log(this.value);
+                console.log(answerChoice);
+                console.log(correctAnswer);
+
+                if (answerChoice === correctAnswer) {
+                    correct.setAttribute("style", "display: inline-block;");
+                    wrong.setAttribute("style", "display: none;");
+                } else if (answerChoice !== correctAnswer) {
+                    quizTimerCount = quizTimerCount - 10;
+                    console.log(quizTimerCount);
+                    correct.setAttribute("style", "display: none;");
+                    wrong.setAttribute("style", "display: inline-block;");
+                };
+            });
+    }
+    console.log("last Question");
+};
